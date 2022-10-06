7e5553c51b4f36512bda798cd46bb12ab4e5e322
NachooLopez <ignacio.fco.lopez@gmail.com>
2022-10-06 19:33:12 -0300

    finish

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+  const { ip: ipaddress } = req;
+  const { 'accept-language': language, 'user-agent': software } = req.headers;
+  res.json({ ipaddress, language, software });
 });
 
 // listen for requests :)
